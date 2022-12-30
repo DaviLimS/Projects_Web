762118f19a51aa8c1c70d4c928f59fa694fd65f7
DaviLimS <davilimasoares1@outlook.com>
2022-12-30 20:43:10 -0300

    main

--- LoginCSS/js/control.js
+++ LoginCSS/js/control.js
@@ -14,9 +14,24 @@ function enter() {
         let p = document.getElementsByClassName('pass1')[0].style.backgroundColor = "rgb(252, 223, 166)";
         let r = document.getElementsByClassName('alert')[0].textContent = "fields are not filled in properly";
     }
+
+    if(b === 'testuser' && z === 'mypassword') {
+        let c = document.getElementsByClassName('testIdUser')[0].style.backgroundColor = "rgb(204, 255, 203)";
+        let p = document.getElementsByClassName('pass1')[0].style.backgroundColor = "rgb(204, 255, 203)";
+        let r = document.getElementsByClassName('alert')[0].textContent = "correct";
+    }
+    if (b != 'testuser' && b != '' && z != 'mypassword' && z != '') {
+        let c = document.getElementsByClassName('testIdUser')[0].style.backgroundColor = "rgb(255,114,118)";
+        let p = document.getElementsByClassName('pass1')[0].style.backgroundColor = "rgb(255,114,118)";
+        let r = document.getElementsByClassName('alert')[0].textContent = "ID or password it's not correct, try again";
+    }
 }
 
-function cancel() {
-    let d = document.getElementsByClassName('testIdUser')[0].textContent = "";
-    let j = document.getElementsByClassName('pass')[0].textContent = "";
+function del() {
+    let d = document.getElementById('idUser').value = '';
+    let j = document.getElementById('password').value = '';
+    let y = document.getElementsByClassName('alert')[0].textContent = ' ';
+
+    let c = document.getElementsByClassName('testIdUser')[0].style.backgroundColor = "white";
+    let p = document.getElementsByClassName('pass1')[0].style.backgroundColor = "white";
 }
