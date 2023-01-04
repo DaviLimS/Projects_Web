38c4b69a8cc6acbf176b52b27df1495be11ca127
DaviLimS <davilimasoares1@outlook.com>
2023-01-03 22:46:33 -0300

    main

--- Projects/Countdown_Timer/js/control.js
+++ Projects/Countdown_Timer/js/control.js
@@ -1,6 +1,8 @@
 function test() {
-    let a = document.getElementById('dateTest');
+    let a = document.getElementById('year');
     let b = a.value;
+    let i = now.getFullYear();
+    let calc1 = b - i;
 
-    alert(b);
+    alert(i);
 }
