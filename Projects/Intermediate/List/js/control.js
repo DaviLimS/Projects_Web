2907fa4467bc1e36f1048347ee5a125c041142a8
DaviLimS <davilimasoares1@outlook.com>
2023-01-28 14:33:10 -0300

    main

---
+++ Projects/Intermediate/List/js/control.js
@@ -0,0 +1,8 @@
+ function color() {
+    let a = document.getElementById("test");
+    let b = document.getElementById("ty");
+    let c = parseFloat(a);
+
+    b.style.backgroundColor = c;
+    alert(c);
+}
