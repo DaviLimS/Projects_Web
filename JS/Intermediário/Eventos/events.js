7d2116f378295a6af6c615015a5a5101ee0b3822
DaviLimS <davilimasoares1@outlook.com>
2023-01-30 20:24:56 -0300

    main

---
+++ JS/Intermediário/Eventos/events.js
@@ -0,0 +1,13 @@
+let a = document.getElementById('IdDoElemento')
+a.addEventListener('click', funcao)
+a.addEventListener('mouseenter', test)
+
+function funcao() {
+    let a = event.keyCode
+    a.fromCharCode()
+}
+
+function test() {
+    a.style.background = 'black'
+    a.style.marginLeft = b
+}
